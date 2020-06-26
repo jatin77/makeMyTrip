63611af0a0a8811c156dd35a548553052b3e04ba
jatin77 <clockstopfountain@gmail.com>
2020-06-26 18:02:27 +0530

    fix

--- src/components/itemsList/item/Item.jsx
+++ src/components/itemsList/item/Item.jsx
@@ -93,7 +93,9 @@ export default function Item({ item }) {
           ))}
         </div>
 
-        <Typography variant="subtitle2"></Typography>
+        <Typography variant="subtitle2">
+          {item.property_type.toUpperCase()}
+        </Typography>
         <Typography variant="body2" color="textSecondary" component="p">
           This impressive paella is a perfect party dish and a fun meal to cook
           together with your guests. Add 1 cup of frozen peas along with the
